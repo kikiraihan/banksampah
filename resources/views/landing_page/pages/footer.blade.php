1dbbf795c4fdc66ac98317c419877d4187a6844e
kikiraihan <mohzulkiflikatili@gmail.com>
2019-08-02 21:00:22 +0800

    baru

--- resources/views/landing_page/pages/footer.blade.php
+++ resources/views/landing_page/pages/footer.blade.php
@@ -4,9 +4,11 @@
             <div class="col-md-12">
                 <div class="border-top">
                     <p>
+
                         Copyright &copy;<script>
                             document.write(new Date().getFullYear());
-                        </script> All rights reserved By Bank Sampah
+                        </script> All rights reserved. <br>
+                        Made with <b>Love</b> By <a href="https://www.facebook.com/astro.raihan">Moh Zulkifli Katili</a>, <a href="https://www.facebook.com/hidayat.chandra.3">Hidayat Chandra</a>, and <a href="https://www.facebook.com/inakcz">Mohamad Ilham Akbar</a>.
                     </p>
                 </div>
             </div>
