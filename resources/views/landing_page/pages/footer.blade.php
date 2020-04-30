12322c960f63151b1889a4fb5503bae4d1471e87
kikiraihan <mohzulkiflikatili@gmail.com>
2020-04-30 21:55:00 +0800

    made by

--- resources/views/landing_page/pages/footer.blade.php
+++ resources/views/landing_page/pages/footer.blade.php
@@ -8,7 +8,8 @@
                         Copyright &copy;<script>
                             document.write(new Date().getFullYear());
                         </script> All rights reserved. <br>
-                        Made with <b>Love</b> By <a href="https://www.facebook.com/astro.raihan">Moh Zulkifli Katili</a>, <a href="https://www.facebook.com/hidayat.chandra.3">Hidayat Chandra</a>, and <a href="https://www.facebook.com/inakcz">Mohamad Ilham Akbar</a>.
+                        Made with <b>Love</b>
+                        {{--  By <a href="https://www.facebook.com/astro.raihan">Moh Zulkifli Katili</a>, <a href="https://www.facebook.com/hidayat.chandra.3">Hidayat Chandra</a>, and <a href="https://www.facebook.com/inakcz">Mohamad Ilham Akbar</a>.  --}}
                     </p>
                 </div>
             </div>
